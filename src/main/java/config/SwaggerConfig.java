bc941722115fea7a8d6b8267c3b245028df071fe
Damorod <ddamrod@gmail.com>
2021-12-06 10:26:21 -0300

    Update SwaggerConfig.java

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -1,7 +1,7 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.ComponentScan;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
