fa7e01470892128b6c3ad191d308073e24323886
Damorod <ddamrod@gmail.com>
2021-12-06 11:11:59 -0300

    update

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import postgresql.Application;
 
 import org.springframework.context.annotation.ComponentScan;
-import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 
@@ -14,7 +13,6 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
