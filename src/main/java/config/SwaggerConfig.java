f92aa871607cadcceae750345debe4b8957c8b15
Damorod <ddamrod@gmail.com>
2021-12-06 10:30:21 -0300

    test

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
+
+import postgresql.Application;
+
 import org.springframework.context.annotation.ComponentScan;
 import java.util.Collections;
 
