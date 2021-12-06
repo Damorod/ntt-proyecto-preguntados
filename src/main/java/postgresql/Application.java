b8f9cb0a4de4ae6dcfee6bfade292a710bc69a03
Damorod <ddamrod@gmail.com>
2021-12-06 10:48:02 -0300

    test

--- src/main/java/postgresql/Application.java
+++ src/main/java/postgresql/Application.java
@@ -1,7 +1,9 @@
 package postgresql;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
