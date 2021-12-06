bc91527fe1064e8fd100e3214f5b6d2aa9944e73
Damorod <ddamrod@gmail.com>
2021-12-06 19:57:58 -0300

    test

--- src/main/java/com/preguntados/config/SwaggerConfig.java
+++ src/main/java/com/preguntados/config/SwaggerConfig.java
@@ -1,39 +1,34 @@
-package config;
+package com.preguntados.config;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import postgresql.Application;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
-	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("controller"))
-				.paths(PathSelectors.any()).build().apiInfo(getApiInfo());
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.preguntados.controller"))
+				.paths(PathSelectors.any()).build().apiInfo(getApiInfo());	
 	}
 
 	private ApiInfo getApiInfo() {
-		return new ApiInfo("Order Service API", "Order Service API Description", "1.0", "http://codmind.com/terms",
-				new Contact("Codmind", "https://codmind.com", "apis@codmind.com"), "LICENSE", "LICENSE URL",
-				Collections.emptyList()
-
-		);
-	}
-	
-	
+		return new ApiInfo(
+			       "REST API", 
+			       "REST description of API.", 
+			       "API TOS", 
+			       "Terms of service", 
+			       new Contact("Rajib Garai", "https://www.linkedin.com/in/rajibgarai90/", "90rajibgarai@gmail.com"), 
+			       "License of API", "API license URL", Collections.emptyList());
+		}
 }
